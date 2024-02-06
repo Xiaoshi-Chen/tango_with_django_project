60818da36d0b75179ecb0060f57b3e253330d475
“Xiaoshi <2742059c@student.gla.ac.uk>
2024-02-06 23:24:36 +0000

    Chapter4 and exercise

--- rango/views.py
+++ rango/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Import the Category model
-from rango.models import Category
 
 def index(request):
-    response = "Rango says hey there partner! "
-    response += '<a href="{% url "rango:about" %}">About</a>'  
-    return HttpResponse(response)
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage matches to {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page.")
+    
+    context_dict = {'your_name': 'Xiaoshi Chen'}  
+    return render(request, 'rango/about.html', context=context_dict)
+
+# Create your views here.
+
