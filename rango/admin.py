46da83b3b29021dbc75370d01af097e1a60f049d
“Xiaoshi <2742059c@student.gla.ac.uk>
2024-02-08 02:40:54 +0000

    Chapter9 and exercise

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,7 @@
 # Register your models here.
 from django.contrib import admin
 from rango.models import Category, Page
+from rango.models import UserProfile
 
 ## 在网页上创建 category and page
 admin.site.register(Category)
@@ -14,3 +15,4 @@ class CategoryAdmin(admin.ModelAdmin):
 
 # Update the registration to include this customised interface
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
